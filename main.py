a0f59c1ef38d257e6e99bb6d39af55516fb82235
clonsbetterupdatethanmojang <156005156+clonsbetterupdatethanmojang@users.noreply.github.com>
2024-03-11 21:10:14 +0100

    made a tiny fix due to the fact I created a game folder

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ class Game():
     The main class, which
     will probably run the UI
     """
-    def __init__(self, pc=Character()):
+    def __init__(self, pc=ch.Character()):
         self.pc = pc # player character
